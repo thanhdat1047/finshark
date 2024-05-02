4fdc11215d99ce012e33b0ee239f1641afb5a1c4
thanhdat1047 <thanhdat1047t@gmail.com>
2024-05-02 18:01:09 +0700

    Async Await Stock

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ff7f3fc5a0aed74561fc0bd21d8a5154d5ca5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f40cce3f8078ee4d92440bf84c81390aac58824d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
